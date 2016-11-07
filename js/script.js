bdbd7510953cc97508ea275094c7956fa3fa03fd
loychenko <andrij.loychenko@gmail.com>
2016-11-07 12:40:33 +0200

    Lending Planet Express

---
+++ js/script.js
@@ -0,0 +1,146 @@
+$(document).ready(function(){
+	var h = $(window).height();
+
+	$(".sld").addClass('animated zoomIn');
+
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".head1").offset().top) {
+			$(".head1").css({visibility:"visible"});
+			$(".head1").eq(0).addClass('animated zoomIn');	
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".head2").offset().top) {
+			$(".head2").css({visibility:"visible"});
+			$(".head2").eq(0).addClass('animated zoomIn');			
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".head3").offset().top) {
+			$(".head3").css({visibility:"visible"});
+			$(".head3").eq(0).addClass('animated zoomIn');	
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".head4").offset().top) {
+			$(".head4").css({visibility:"visible"});
+			$(".head4").eq(0).addClass('animated zoomIn');	
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".head5").offset().top) {
+			$(".head5").css({visibility:"visible"});
+			$(".head5").eq(0).addClass('animated zoomIn');	
+		} 		
+	});
+
+
+
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".welcom1").offset().top) {
+			$(".welcom1").css({visibility:"visible"});
+			$(".welcom1").eq(0).addClass('animated bounceInLeft');	
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".welcom2").offset().top) {
+			$(".welcom2").css({visibility:"visible"});
+			$(".welcom2").eq(0).addClass('animated bounceInRight');			
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".welcom3").offset().top) {
+			$(".welcom3").css({visibility:"visible"});
+			$(".welcom3").eq(0).addClass('animated bounceInLeft');	
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".welcom4").offset().top) {
+			$(".welcom4").css({visibility:"visible"});
+			$(".welcom4").eq(0).addClass('animated bounceInRight');	
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".gallery1").offset().top) {
+			$(".gallery1").css({visibility:"visible"});
+			$(".gallery1").eq(0).addClass('animated bounceInRight');	
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".gallery2").offset().top) {
+			$(".gallery2").css({visibility:"visible"});
+			$(".gallery2").eq(0).addClass('animated bounceInLeft');	
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".gallery3").offset().top) {
+			$(".gallery3").css({visibility:"visible"});
+			$(".gallery3").eq(0).addClass('animated bounceInRight');	
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".gallery4").offset().top) {
+			$(".gallery4").css({visibility:"visible"});
+			$(".gallery4").eq(0).addClass('animated bounceInLeft');	
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".gallery5").offset().top) {
+			$(".gallery5").css({visibility:"visible"});
+			$(".gallery5").eq(0).addClass('animated bounceInRight');	
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".gallery6").offset().top) {
+			$(".gallery6").css({visibility:"visible"});
+			$(".gallery6").eq(0).addClass('animated bounceInLeft');	
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".about-me2").offset().top) {
+			$(".about-me2").css({visibility:"visible"});
+			$(".about-me2").eq(0).addClass('animated bounceInRight');	
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".about-me1").offset().top) {
+			$(".about-me1").css({visibility:"visible"});
+			$(".about-me1").eq(0).addClass('animated bounceInLeft');	
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".blog1").offset().top) {
+			$(".blog1").css({visibility:"visible"});
+			$(".blog1").eq(0).addClass('animated bounceInLeft');	
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".blog2").offset().top) {
+			$(".blog2").css({visibility:"visible"});
+			$(".blog2").eq(0).addClass('animated bounceInRight');			
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".blog3").offset().top) {
+			$(".blog3").css({visibility:"visible"});
+			$(".blog3").eq(0).addClass('animated bounceInLeft');	
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".blog4").offset().top) {
+			$(".blog4").css({visibility:"visible"});
+			$(".blog4").eq(0).addClass('animated bounceInRight');	
+		} 		
+	});
+	$(window).scroll(function(){
+		if ( ($(this).scrollTop()+h) >= $(".footer").offset().top) {
+			$(".footer").css({visibility:"visible"});
+			$(".footer").eq(0).addClass('animated bounceInRight');	
+		} 		
+	});
+	
+	
+});
+
+
+
